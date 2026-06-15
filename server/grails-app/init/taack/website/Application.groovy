ffebce20021e8caa27ad877268ff739c302b9d01
adrien <adrien.guichard@citel.fr>
2026-06-16 00:49:14 +0200

    Sync deps versions with TaackUI 0.7.4, deactivate Spring Dev Tools (which interfere with debug)

--- server/grails-app/init/taack/website/Application.groovy
+++ server/grails-app/init/taack/website/Application.groovy
@@ -8,6 +8,7 @@ import taack.ui.TaackUiConfiguration
 @CompileStatic
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false")
         TaackUiConfiguration.root = TaackUiConfiguration.home + '/intranetFilesDev'
         GrailsApp.run(Application, args)
     }
